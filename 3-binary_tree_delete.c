5b64a0afb4d8ae467968cb0adec27ef1fd05c3a5
Karima Touhami <113696378+KarimaTouhami@users.noreply.github.com>
2024-02-28 22:50:31 +0100

    Updated

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,13 +1,14 @@
 #include "binary_trees.h"
 /**
- * binary_tree_delete - deletes an entire binary tree
- * @tree: pointer to the root node of the tree to delete
+ * binary_tree_delete -  creates a binary tree node.
+ * @tree: tree root.
+ * Return: none
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
-    free(tree);
+	if (!tree)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
